10bd8578b70fc4c24e1e008e8d6b3cdadfc6144a
dirstart <835041058@qq.com>
2018-07-08 13:44:10 +0800

    star-item debug and ok

--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -9,6 +9,7 @@
   <div class="star" :class="starType">
     <span v-for="(item,index) in styleArray"
       :class="item"
+      class="star-item"
       :key="index"></span>
   </div>
 </template>
@@ -55,7 +56,7 @@ export default {
       return result;
     }
   },
-  mounted: {},
+  mounted() {},
   methods: {}
 };
 </script>
@@ -67,42 +68,45 @@ export default {
       display inline-block
       background-repeat no-repeat
     &.star-48
-      width 20px
-      height 20px
-      margin-right 22px
-      background-size 20px 20px
-      &:last-child
-        margin-right 0
-      &.on
-        bg-image('./img/star48_on')
-      &.half
-        bg-image('./img/star48_half')
-      &.off
-        bg-image('./img/star48_off')
+      .star-item
+        width 20px
+        height 20px
+        margin-right 22px
+        background-size 20px 20px
+        &:last-child
+          margin-right 0
+        &.on
+          bg-image('./img/star48_on')
+        &.half
+          bg-image('./img/star48_half')
+        &.off
+          bg-image('./img/star48_off')
     &.star-36
-      width 15px
-      height 15px
-      margin-right 6px
-      background-size 15px 15px
-      &:last-child
-        margin-right 0
-      &.on
-        bg-image('./img/star36_on')
-      &.half
-        bg-image('./img/star36_half')
-      &.off
-        bg-image('./img/star36_off')
+      .star-item
+        width 15px
+        height 15px
+        margin-right 6px
+        background-size 15px 15px
+        &:last-child
+          margin-right 0
+        &.on
+          bg-image('./img/star36_on')
+        &.half
+          bg-image('./img/star36_half')
+        &.off
+          bg-image('./img/star36_off')
     &.star-24
-      width 10px
-      height 10px
-      margin-right 3px
-      background-size 10px 10px
-      &:last-child
-        margin-right 0
-      &.on
-        bg-image('./img/star24_on')
-      &.half
-        bg-image('./img/star24_half')
-      &.off
-        bg-image('./img/star24_off')
+      .star-item
+        width 10px
+        height 10px
+        margin-right 3px
+        background-size 10px 10px
+        &:last-child
+          margin-right 0
+        &.on
+          bg-image('./img/star24_on')
+        &.half
+          bg-image('./img/star24_half')
+        &.off
+          bg-image('./img/star24_off')
 </style>
